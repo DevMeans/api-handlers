ee7982338c04d29ef35145662996a244cc6355ad
Devent <tavata.canales@gmail.com>
2024-01-13 13:55:22 -0500

    se añadio nueva appi seeed

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,17 @@
+import prisma from '@/app/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(request: Request) {
+    await prisma.todo.deleteMany()
+    const todo = await prisma.todo.createMany({
+        data: [
+        { description: 'Píedra del alma',complete:true },
+        { description: 'Píedra del poder'},
+        { description: 'Píedra del tiempo'},
+        { description: 'Píedra del espacio'},
+        { description: 'Píedra del realidad'}
+    ]
+    })
+    console.log(todo)
+    return NextResponse.json({ message: 'Seed execute' })
+}
