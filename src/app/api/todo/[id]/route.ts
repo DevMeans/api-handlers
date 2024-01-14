fd37d3c054677abe75bb8eefe6d319363bca5c89
Devent <tavata.canales@gmail.com>
2024-01-14 08:34:33 -0500

    busqueda por id añadida

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -1,8 +1,12 @@
+import prisma from '@/app/lib/prisma';
+import { Segment } from 'next/dist/server/app-render/types';
 import { NextResponse, NextRequest } from 'next/server'
 
-export async function GET(request: Request) { 
-
-  return new Response(JSON.stringify({
-    message: 'Hello World'
-  }), { status: 200 } );
+export async function GET(request: Request,{params}:Segment) { 
+  const { id } =params
+  const todo = await prisma.todo.findFirst({where:{id}})
+  if(!todo){
+    return NextResponse.json({message:`Todo con el id ${id} no existe`},{status:404})
+  }
+  return NextResponse.json(todo)
 }
