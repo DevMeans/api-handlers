e73c98d0fa2cecea05d694c7f2b157afc1ebbc1a
Devent <tavata.canales@gmail.com>
2024-01-14 09:47:41 -0500

    creado el post y agregado yup

--- src/app/api/todo/[id]/route.ts
+++ src/app/api/todo/[id]/route.ts
@@ -10,3 +10,4 @@ export async function GET(request: Request,{params}:Segment) {
   }
   return NextResponse.json(todo)
 }
+
--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/app/lib/prisma'
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url)
     const take = Number(searchParams.get('take') ?? '10')
@@ -15,4 +16,20 @@ export async function GET(request: Request) {
         skip
     })
     return NextResponse.json(todos)
+}
+
+const postSchema = yup.object({
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false)
+})
+
+export async function POST(request: Request) {
+    try {
+        const {complete,description} = await postSchema.validate(await request.json())
+        const todo = await prisma.todo.create({ data: {complete,description} })
+        return NextResponse.json(todo)
+    } catch (error) {
+        return NextResponse.json(error,{status:400})
+    }
+
 }
