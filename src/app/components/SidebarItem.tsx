f97d06de4d162750ecd37f475b579287d2dc2778
unknown <tavata.canale@gmail.com>
2024-01-16 17:59:56 -0500

    Se añadio sidebarItem

--- src/app/components/SidebarItem.tsx
+++ src/app/components/SidebarItem.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
+import { CiBookmarkCheck } from 'react-icons/ci'
 
 export const SidebarItem = () => {
-  return (
-    <div>SidebarItem</div>
-  )
+    return (
+        <li>
+            <Link href="/dashboard/rest-todos" className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+                <CiBookmarkCheck size={30} />
+                <span className="-mr-1 font-medium">Dashboard</span>
+            </Link>
+        </li>
+
+    )
 }
