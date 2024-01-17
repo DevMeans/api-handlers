6427a40d8becdd941d3c55a9b7d8505580c00a8a
unknown <tavata.canale@gmail.com>
2024-01-17 09:20:06 -0500

    TodoItem.Module.css añadido se agregaron funcionalidades

--- src/app/todos/components/TodoItem.tsx
+++ src/app/todos/components/TodoItem.tsx
@@ -1,12 +1,24 @@
 import { Todo } from '@prisma/client'
 import React from 'react'
-
-interface Props{
-    todo:Todo
+import styles from './TodoItem.module.css'
+import { IoCheckboxOutline, IoSquare, IoSquareOutline } from 'react-icons/io5'
+interface Props {
+  todo: Todo
 }
 
-export const TodoItem = ({todo}:Props) => {
+export const TodoItem = ({ todo }: Props) => {
   return (
-    <div>{todo.description}</div>
+    <div className={todo.complete ? styles.todoDone : styles.todoPending}>
+      <div className='flex flex-col sm:flex-row justify-start items-center gap-4'>
+        <div className={`flex p-2 rounded-md cursor-pointer hover:bg-opacity-60 ${todo.complete ? 'bg-blue-100' :'bg-red-100'} `}>
+         {
+          todo.complete ? <IoCheckboxOutline size={30}></IoCheckboxOutline> : <IoSquareOutline size={30}></IoSquareOutline>
+         }
+        </div>
+        <div className='text-center sm:text-left'>
+          {todo.description}
+        </div>
+      </div>
+    </div>
   )
 }
