b942f3da7756b7bef3b14fc4d25d615558a3800f
unknown <tavata.canale@gmail.com>
2024-01-18 07:36:51 -0500

    se modifica la pagina serverActions

--- src/app/dashboard/server-todos/page.tsx
+++ src/app/dashboard/server-todos/page.tsx
@@ -1,8 +1,24 @@
+import prisma from "@/app/lib/prisma";
+import { TodosGrid } from '../../todos/components/TodosGrid';
+import { NewTodo } from "@/app/todos";
 
-export default function ServerTodosPage() {
+
+
+export const metadata = {
+  title: 'Server Action',
+  description: 'Listado de TODO',
+};
+
+export default async function RestTodos() {
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } })
   return (
-    <div>
-      <h1>Server Page</h1>
-    </div>
+    <>
+      <span className="text-3xl mb-10">Server Actions</span>
+      <div className="w-full px-3 mx-5 mb-5">
+        <NewTodo />
+      </div>
+
+      <TodosGrid todos={todos} />
+    </>
   );
 }
