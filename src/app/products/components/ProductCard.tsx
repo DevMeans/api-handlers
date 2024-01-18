ca9e500d4c884d1d82bee7451080684a6d2a8196
unknown <tavata.canale@gmail.com>
2024-01-18 14:03:56 -0500

    funcion añadir producto al cookie funcional

--- src/app/products/components/ProductCard.tsx
+++ src/app/products/components/ProductCard.tsx
@@ -1,8 +1,9 @@
 // https://tailwindcomponents.com/component/e-commerce-product-card
-
+'use client'
 import Image from "next/image"
 import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5"
 import { Star } from './Star';
+import { addProductToCard } from "@/app/shoping-cart/actions/actions";
 interface Props {
     id: string;
     name: string;
@@ -12,6 +13,9 @@ interface Props {
 }
 
 export const ProductCard = ({ id, name, price, rating, image }: Props) => {
+    const  onAddToCard =()=>{
+        addProductToCard(id)
+    }
     return (
         <div className="bg-white shadow rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-100">
 
@@ -55,6 +59,7 @@ export const ProductCard = ({ id, name, price, rating, image }: Props) => {
 
                     <div className="flex">
                         <button
+                        onClick={onAddToCard}
                             className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <IoAddCircleOutline size={25} />
                         </button>
---
+++ src/app/shoping-cart/actions/actions.ts
@@ -0,0 +1,19 @@
+import { getCookie, hasCookie, setCookie } from "cookies-next"
+
+export const getCookieCart = (): { [id: string]: number } => {
+    if (hasCookie('cart')) {
+        const coockiecard = JSON.parse(getCookie('cart') as string ?? '{}');
+        return coockiecard
+    }
+    return {}
+}
+export const addProductToCard = (id: string) => {
+    
+    const cookiecart = getCookieCart()
+    if (cookiecart[id]) {
+        cookiecart[id] = cookiecart[id] + 1
+    }else{
+        cookiecart[id] = 1
+    }
+    setCookie('cart',JSON.stringify(cookiecart))
+}
