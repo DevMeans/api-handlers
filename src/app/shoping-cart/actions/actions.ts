e1236e8f54e2e2331821dec81c5ef39f2765dcc2
unknown <tavata.canale@gmail.com>
2024-01-18 18:37:53 -0500

    Se añadio funcionalidad

--- src/app/shoping-cart/actions/actions.ts
+++ src/app/shoping-cart/actions/actions.ts
@@ -12,13 +12,22 @@ export const addProductToCard = (id: string) => {
     const cookiecart = getCookieCart()
     if (cookiecart[id]) {
         cookiecart[id] = cookiecart[id] + 1
-    }else{
+    } else {
         cookiecart[id] = 1
     }
-    setCookie('cart',JSON.stringify(cookiecart))
+    setCookie('cart', JSON.stringify(cookiecart))
 }
-export const removeProductFromCart=(id:string)=>{
+export const removeProductFromCart = (id: string) => {
     const cookiecart = getCookieCart()
     delete cookiecart[id]
-    setCookie('cart',JSON.stringify(cookiecart))
+    setCookie('cart', JSON.stringify(cookiecart))
+}
+export const removeSingleitemFromCart = (id: string, valor: number) => {
+    const cookiecart = getCookieCart()
+    if(!cookiecart[id]) return;
+    cookiecart[id] = cookiecart[id] + valor
+    if(cookiecart[id]<=0){
+        delete cookiecart[id]
+    }
+    setCookie('cart', JSON.stringify(cookiecart))
 }
